fd2a8a986cb79a72dc2d8c8db12f5ba1c402a3f4
sisaha9 <sisahawork@gmail.com>
2024-03-13 19:47:53 -0700

    update

--- donkeycar/parts/actuator.py
+++ donkeycar/parts/actuator.py
@@ -215,7 +215,7 @@ class GTK():
 
         throttle = throttle * self.speed_multiplier
 
-        print(f"throttle: {throttle}, angle: {angle}")
+        # print(f"throttle: {throttle}, angle: {angle}")
 
         # Convert the floats to bytes
         throttle = self.__float_to_byte(throttle)
