524ce0a14f4cec201fad877241be7b5febb7b68f
nbetts2020 <114270814+nbetts2020@users.noreply.github.com>
2024-05-08 20:04:23 -0700

    Update oak_d_camera.py

--- donkeycar/parts/oak_d_camera.py
+++ donkeycar/parts/oak_d_camera.py
@@ -321,7 +321,7 @@ class OakDCamera:
             if self.queue_left is not None and self.queue_left.has():
                 data_left = self.queue_left.get()
                 self.frame_left = data_left.getCvFrame()
-                self.frame_left = np.clip(self.frame_left >> 8, 0, 255).astype(np.uint8)  # Bit-shift and convert to uint8
+                self.frame_left = cv2.resize(self.frame_left, (160, 120), interpolation=cv2.INTER_AREA)
                 self.frame_left = np.stack((self.frame_left,)*3, axis=-1)  # Convert single channel to three-channel RGB
                 print(self.frame_left, "boooo", self.frame_left.shape)
                 # self.frame_left = np.moveaxis(image_data_xout_left,0,-1)
@@ -330,7 +330,7 @@ class OakDCamera:
             if self.queue_right is not None and self.queue_right.has():
                 data_right = self.queue_right.get()
                 self.frame_right = data_right.getCvFrame()
-                self.frame_right = np.clip(self.frame_right >> 8, 0, 255).astype(np.uint8)  # Bit-shift and convert to uint8
+                self.frame_right = cv2.resize(self.frame_right, (160, 120), interpolation=cv2.INTER_AREA)
                 self.frame_right = np.stack((self.frame_right,)*3, axis=-1)  # Convert single channel to three-channel RGB
 
                 # self.frame_right = np.moveaxis(self.frame_right,0,-1)
@@ -382,7 +382,7 @@ class OakDCamera:
             if self.queue_left is not None and self.queue_left.has():
                 data_left = self.queue_left.get()
                 self.frame_left = data_left.getFrame()
-                self.frame_left = np.clip(self.frame_left >> 8, 0, 255).astype(np.uint8)  # Bit-shift and convert to uint8
+                self.frame_left = cv2.resize(self.frame_left, (160, 120), interpolation=cv2.INTER_AREA)
                 self.frame_left = np.stack((self.frame_left,)*3, axis=-1)  # Convert single channel to three-channel RGB
                 print(self.frame_left, "boooo", self.frame_left.shape)
                 # self.frame_left = np.moveaxis(image_data_xout_left,0,-1)
@@ -391,7 +391,7 @@ class OakDCamera:
             if self.queue_right is not None and self.queue_right.has():
                 data_right = self.queue_right.get()
                 self.frame_right = data_right.getFrame()
-                self.frame_right = np.clip(self.frame_right >> 8, 0, 255).astype(np.uint8)  # Bit-shift and convert to uint8
+                self.frame_right = cv2.resize(self.frame_right, (160, 120), interpolation=cv2.INTER_AREA)
                 self.frame_right = np.stack((self.frame_right,)*3, axis=-1)  # Convert single channel to three-channel RGB
 
                 # self.frame_right = np.moveaxis(self.frame_right,0,-1)
